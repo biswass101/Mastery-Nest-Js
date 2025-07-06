7921d2a6bd1749ef156e069bd18f9acc9ca10800
Service2Returnhex <service2.returnhex@gmail.com>
2025-07-07 01:18:45 +0600

    feat(user): add query parameter reading from req url

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,25 @@
-import { Controller, Get, Param, Post } from "@nestjs/common";
+import { Controller, Get, Param, ParseIntPipe, Post, Query } from "@nestjs/common";
 import { UsersService } from "./users.service";
 
 @Controller('users') //users url
 export class UsersController {
 
     @Get() // Get routing decorator
-    getUsers() {
+    // @Query('id')
+    getUsers(@Query() query: any) {
         const usersService = new UsersService();
+        console.log(query.isMarried);
+        const isMarried = query.isMarried === 'true';
+        if(query.isMarried !== undefined) return usersService.getAllUsers().filter(u => u.isMarried === isMarried);
         return usersService.getAllUsers();
         // return "You made a GET request to get all users!"
     }
     // :id/:name/:gender
     @Get(':id') // ? means this parameter is optional
-    getUserById(@Param('id') id: any) {
+    getUserById(@Param('id', ParseIntPipe) id: any) {
         const usersService = new UsersService();
-        return usersService.getUserById(+id);
+        // console.log(typeof(id), id);
+        return usersService.getUserById(id);
     }
 
     @Post() // Post routing decorator
