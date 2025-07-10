7b4c1390637e6564f394f6423c1dce47ef045a20
biswass101 <biswassnaeemcse@gmail.com>
2025-07-10 11:29:02 +0600

    chore(database): connect NestJS app to PostgreSQL using TypeORM for synchronize connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
 
 @Module({
-  imports: [UsersModule, TweetModule],
+  imports: [UsersModule, TweetModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    entities: [],
+    synchronize: true, // for dev mode only
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '2025Niloy',
+    database: 'NestJS'
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
