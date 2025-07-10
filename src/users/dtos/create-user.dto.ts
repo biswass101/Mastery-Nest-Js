ca0dc4f060ced47fb317e4607771d7f104953c6e
biswass101 <biswassnaeemcse@gmail.com>
2025-07-10 14:26:41 +0600

    chore(entity): configure the db entity column

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsBoolean, IsEmail, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, MinLength } from "class-validator";
+import { IsBoolean, IsEmail, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
 import { Gender } from "src/common/enums/gender.enum";
 
 export class CreateUserDto {
@@ -7,24 +7,29 @@ export class CreateUserDto {
     @IsString({message: "First name should be a string value"})
     @IsNotEmpty()
     @MinLength(3, {message: "First name  should be at least 3 charcter long"})
+    @MaxLength(100)
     firstName: string;
 
     @IsString({message: "Last name  be a string value"})
     @IsNotEmpty()
     @MinLength(3, {message: "Last should be at least 3 charcter long"})
+    @MaxLength(100)
     lastName: string;
 
     @IsString()
     @IsOptional()
+    @MaxLength(10)
     gender?: string;
 
     @IsEmail()
     @IsNotEmpty()
+    @MaxLength(100)
     email: string;
 
     @IsString()
     @IsNotEmpty()
     @MinLength(5)
+    @MaxLength(100)
     password: string
 
 }
